3f6249679c1dda9b7dfd0955a18dc3118ce8b4e2
gilgamesh7 <felidaesrule@yahoo.com>
2022-11-17 18:39:16 +1300

    change directory structure and add tests

---
+++ src/tests/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+from starlette.testclient import TestClient
+
+from app.main import app
+
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client
+
+def test_ping(test_app):
+    response = test_app.get("/ping")
+
+    assert response.status_code == 200
+    assert response.json() == {"ping": "pong!"}
