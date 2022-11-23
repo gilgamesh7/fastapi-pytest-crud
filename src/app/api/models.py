8406ab8ecdf05c7280ab2cd57310922ca9fcd4c9
gilgamesh7 <felidaesrule@yahoo.com>
2022-11-24 09:25:48 +1300

    Add router, handler for POST Create

--- src/app/api/models.py
+++ src/app/api/models.py
@@ -3,4 +3,6 @@ from pydantic import BaseModel
 class NoteSchema(BaseModel):
     title: str
     description: str
-    
+
+class NoteDB(NoteSchema):
+    id: int
--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 
-from app.api import ping
+from app.api import ping, notes
 from app.db import engine, database, metadata
 
 app = FastAPI()
@@ -17,6 +17,7 @@ async def shutdown():
     await database.disconnect()
 
 app.include_router(ping.router)
+app.include_router(notes.router, prefix="/notes", tags=["notes"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
